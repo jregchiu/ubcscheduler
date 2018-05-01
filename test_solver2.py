f8939061ac47f0b6691e44632b4abbb7e439a921
Jeremy Chiu <8358221+jregchiu@users.noreply.github.com>
2018-04-30 17:13:16 -0700

    try winter CPSC courses

--- test_solver2.py
+++ test_solver2.py
@@ -1,34 +1,35 @@
-from json import load
+from json import loads
 from ortools.constraint_solver import pywrapcp
 
 def main():
     terms = ('1', '2')
     days = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri')
     hours = ('7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30')
-    activities_filter = ('Lecture', 'Laboratory', 'Tutorial', 'Seminar', 'Discussion')
-    filters = ('Full', 'Waiting List')
+    section_keys = ('course', 'activity', 'term', 'days', 'start', 'end')
+    filters = ('Waiting List')
+    long_weekends = ('Mon', 'Wed', 'Fri')
 
-    courses_to_take = ('CPSC 213', 'CPSC 221')
+    courses_to_take = ('CPSC 310', 'CPSC 304')
 
     # read in course data
-    with open('sample_data/cpsc18s.json') as f:
-        all_courses = load(f)
-
     courses = {}
     sections = {}
-    for c in [c for c in all_courses if c['code'] in courses_to_take]:
-        activity_set = set()
-        courses[c['code']] = c
-        for s in c['sections']:
-            if s['activity'] in activities_filter:
-                # if ('status' in s and s['status'] not in filters) or 'status' not in s:
-                    sections[s['code']] = s
-                    activity_set.add(s['activity'])
-                    del s['code']
-        assert len(activity_set) == int(c['activities'])
-        c['activity_set'] = activity_set
-        del c['code']
-        del c['sections']
+    with open('sample_data/cpsc2018w.jl') as f:
+        for jsonline in f:
+            c = loads(jsonline)
+            code = c['code']
+            if code in courses_to_take:
+                activity_set = set()
+                courses[code] = c
+                for s in c['sections']:
+                    if all (k in s for k in section_keys):
+                        if s['activity'] not in filters:
+                            sections[s['code']] = s
+                            activity_set.add(s['activity'])
+                            del s['code']
+                c['activities'] = activity_set
+                del c['code']
+                del c['sections']
 
     # creates the constraint solver
     solver = pywrapcp.Solver('test_schedule_courses2')
@@ -38,6 +39,10 @@ def main():
     for s in sections:
         solver_sections[s] = solver.BoolVar('{}'.format(s))
     solver_sections_flat = [solver_sections[s] for s in sections]
+
+    # long weekend constraint
+    for s in {s: sections[s] for s in sections if any (d in sections[s]['days'] for d in long_weekends)}:
+        solver.Add(solver_sections[s] == 0)
 
     # create the constraints
     term_day_time = {}
@@ -57,14 +62,14 @@ def main():
 
     course_activity = {}
     for c in courses:
-        activities = courses[c]['activity_set']
+        activities = courses[c]['activities']
         for a in activities:
             course_activity[(c, a)] = solver.IntVar(0, len(sections), '{0}, {1}'.format(c, a))
             sections_to_sum = {s: sections[s] for s in sections if c == sections[s]['course'] and a == sections[s]['activity']}
             solver.Add(course_activity[(c, a)] == solver.Sum([solver_sections[s] for s in sections_to_sum]))
             solver.Add(course_activity[(c, a)] >= 0)
             solver.Add(course_activity[(c, a)] <= 1)
-        solver.Add(solver.Sum([course_activity[(c, a)] for a in activities]) == int(courses[c]['activities']))
+        solver.Add(solver.Sum([course_activity[(c, a)] for a in activities]) == len(courses[c]['activities']))
 
     # create the decision builder
     db = solver.Phase(solver_sections_flat, solver.CHOOSE_FIRST_UNBOUND, solver.ASSIGN_MIN_VALUE)

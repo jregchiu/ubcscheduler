7754023f46f5d897a2e6da5c352f4d9f065c5351
Jeremy Chiu <jregchiu@gmail.com>
2018-05-27 11:59:13 -0700

    change to use postgresql ARRAY, text columns, simplify import_sections.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
 from app import db
+from sqlalchemy.dialects import postgresql as pg
 
 class Course(db.Model):
     __tablename__ = 'courses'
 
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String, index=True, unique=True, nullable=False)
-    activities = db.Column(db.ARRAY(db.String), nullable=False)
+    code = db.Column(db.Text, index=True, unique=True, nullable=False)
+    activities = db.Column(pg.ARRAY(db.Text), nullable=False)
     sections = db.relationship('Section', backref='course', lazy=True)
 
     def __repr__(self):
@@ -17,12 +18,12 @@ class Section(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
     year = db.Column(db.Integer, index=True, nullable=False)
-    session = db.Column(db.String, index=True, nullable=False)
-    code = db.Column(db.String, nullable=False)
-    status = db.Column(db.String)
-    activity = db.Column(db.String, nullable=False)
-    term = db.Column(db.ARRAY(db.String), index=True, nullable=False)
-    days = db.Column(db.ARRAY(db.String), index=True, nullable=False)
+    session = db.Column(db.Text, index=True, nullable=False)
+    code = db.Column(db.Text, nullable=False)
+    status = db.Column(db.Text)
+    activity = db.Column(db.Text, nullable=False)
+    term = db.Column(pg.ARRAY(db.Integer), index=True, nullable=False)
+    days = db.Column(pg.ARRAY(db.Text), index=True, nullable=False)
     start = db.Column(db.Time, index=True, nullable=False)
     end = db.Column(db.Time, index=True, nullable=False)
 
--- import_sections.py
+++ import_sections.py
@@ -13,15 +13,13 @@ with open(s) as f:
         s = loads(line)
         if s['course'] not in courses:
             c = Course(code=s['course'], activities=[s['activity']])
-            sec = Section(course=c, year=int(s['year']), session=s['session'], code=s['code'], status=s.get('status'), activity=s['activity'], term=s['term'], days=s['days'], start=s['start'], end=s['end'])
             courses[s['course']] = c
-            db.session.add(sec)
         else:
             c = courses[s['course']]
             if s['activity'] not in c.activities:
                 c.activities.append(s['activity'])
-            sec = Section(course=c, year=int(s['year']), session=s['session'], code=s['code'], status=s.get('status'), activity=s['activity'], term=s['term'], days=s['days'], start=s['start'], end=s['end'])
-            db.session.add(sec)
+        sec = Section(course=c, year=int(s['year']), session=s['session'], code=s['code'], status=s.get('status'), activity=s['activity'], term=[int(t) for t in s['term']], days=s['days'], start=s['start'], end=s['end'])
+        db.session.add(sec)
 
 for c in courses.values():
     db.session.add(c)
